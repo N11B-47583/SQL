bc655da84abf92b66ec7ac44e5ad5d67d13970ec
pkrzeminski <56222197+N11B-47583@users.noreply.github.com>
2022-05-24 15:24:05 +0200

    Create getAge.sql
    The function returns the exact current age.

---
+++ getAge.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION getAge (@birthDay date)
+RETURNS int 
+AS 
+BEGIN
+RETURN (
+        SELECT
+          CASE WHEN DATEADD(YEAR, DATEDIFF(YEAR,@birthDay, GETDATE()), @birthDay) > GETDATE()
+          THEN DATEDIFF(YEAR, @birthDay, GETDATE()) -1
+          ELSE DATEDIFF(YEAR, @birthDay, GETDATE())
+          END
+          )
+END
+GO
