fba964abc244e809d88fc1faada61f25c7bd240a
pkrzeminski <56222197+N11B-47583@users.noreply.github.com>
2022-05-24 15:10:32 +0200

    Update RamToAssgin.sql

--- RamToAssgin.sql
+++ RamToAssgin.sql
@@ -3,7 +3,7 @@ DECLARE @Tmp table (
 					RAM varchar(50)
 					)
 
--- Pobiera całkowią pamięć z serwera.
+-- Get total physical memory from the server.
 DECLARE @query varchar(1000) = 'powershell.exe "(Get-CimInstance Win32_PhysicalMemory | Measure-Object -Property capacity -Sum).sum / 1gb"'
 
 INSERT INTO @Tmp
@@ -16,20 +16,20 @@ WHERE Id = 1
 
 DECLARE @Mod8RAM	int
    SET  @Mod8RAM	= @RAM - @RAM%8 
-DECLARE @Jm			char(2)
+DECLARE @Unit		char(2)
 DECLARE @counter bigint
 
--- Podaj jednostkę w jakiej ma zostać przeliczone przypisanie [GB MB KB B]
+-- Enter the unit in which the assignment is to be converted [GB MB KB B]
 
-SET @Jm		 = 'GB'
-SET @counter =  CASE @Jm 
+SET @Unit		 = 'GB'
+SET @counter =  CASE @Unit 
 				WHEN 'GB' THEN 1		  -- Gigabyte
 				WHEN 'MB' THEN 1024		  -- Megabyte
 				WHEN 'KB' THEN 1048576    -- Kilobyte
 				WHEN 'B'  THEN 1073741824 -- Byte
 				ELSE 1 END
 
--- SQLServer >= 2014 -- Funkcja IIF oraz CONCAT dos
+-- SQLServer >= 2014 --  IIF / CONCAT - SQLServer 2014 and higher
 --SELECT CONCAT(IIF(@RAM>=16,(@RAM-((@Mod8RAM - 16)/8 + 4))*@counter, @RAM-4),' ',@Jm) AS RamToAssign
 
-SELECT CASE WHEN @RAM >= 16 THEN CONVERT(varchar(20),(@RAM-((@Mod8RAM - 16)/8 + 4))*@counter) ELSE CONVERT(varchar(20),@RAM-4) END + ' ' + @Jm AS RamToAssign
+SELECT CASE WHEN @RAM >= 16 THEN CONVERT(varchar(20),(@RAM-((@Mod8RAM - 16)/8 + 4))*@counter) ELSE CONVERT(varchar(20),@RAM-4) END + ' ' + @Unit AS RamToAssign
