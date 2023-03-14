f118802c2849e523b5625418c971f648456c7184
pkrzeminski <56222197+N11B-47583@users.noreply.github.com>
2023-03-14 14:12:59 +0100

    Create StringSpit.sql
    The function splits the string based on the separator. For example:
    SELECT * FROM dbo.StringSplit('I,am,a,computer,scientist', DEFAULT) returns:
    
    result
    ------
    I
    am
    a
    computer
    scientist

---
+++ StringSpit.sql
@@ -0,0 +1,24 @@
+CREATE OR ALTER FUNCTION dbo.StringSplit (@string varchar(1024), @delimiter char(1) = ',')
+RETURNS @result TABLE (
+						result varchar(100)
+						)
+AS
+BEGIN
+
+DECLARE @start int = 1
+DECLARE @pos   int = CHARINDEX(@delimiter, @string, @start)
+
+IF @pos = 0 
+INSERT INTO @result SELECT @String
+
+WHILE @start <= @pos 
+BEGIN
+	INSERT INTO @result
+		SELECT SUBSTRING(@string, @start, @pos-@start) AS string
+		SET @start = @pos + 1
+		SET @pos = CASE WHEN CHARINDEX(@Delimiter, @String, @start) = 0 THEN LEN(@string) + 1 ELSE CHARINDEX(@delimiter, @string, @start)  END
+	END
+RETURN
+END
+
+
